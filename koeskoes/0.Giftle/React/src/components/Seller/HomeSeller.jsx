4de89b02e58a4e1072013e7f26c721c9a8d9446b
Sven <S.vanEe1@student.han.nl>
2021-11-15 15:16:44 +0100

    Prettier

--- koeskoes/0.Giftle/React/src/components/Seller/HomeSeller.jsx
+++ koeskoes/0.Giftle/React/src/components/Seller/HomeSeller.jsx
@@ -4,11 +4,9 @@ function HomeSeller() {
     <div className="vertical-center colored-background">
       <div className="container text-center rounded p-3 bg-light">
         <h1>Welkom op Giftle!</h1>
-        <p>
+        <p className="mt-3 mb-5">
           Wilt u een video meesturen met uw cadeau? Als u een video heeft, kunt
           u deze uploaden. Er kan ook een video worden opgenomen.
-          <br />
-          <br />
         </p>
         <button className="btn btn-primary mx-4">Opnemen</button>
         <button className="btn btn-primary mx-4">Uploaden</button>
--- koeskoes/0.Giftle/React/src/index.js
+++ koeskoes/0.Giftle/React/src/index.js
@@ -1,10 +1,10 @@
-import App from './components/App';
+import App from "./components/App";
 import "./styles/index.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 import React from "react";
 import ReactDOM from "react-dom";
 
-import 'bootstrap/dist/css/bootstrap.min.css';
+import "bootstrap/dist/css/bootstrap.min.css";
 import "./styles/index.css";
 
 import * as Redux from "redux";
