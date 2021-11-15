196d260c57c24d607c06d18a8b243cd3f5d656e0
Sven <S.vanEe1@student.han.nl>
2021-11-15 14:15:03 +0100

    Routes working

---
+++ koeskoes/0.Giftle/React/src/components/Receiver/HomeReceiver.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function HomeReceiver() {
+  return (
+    <div>
+      <h1>HOME - Receiver</h1>
+    </div>
+  );
+}
+
+export default HomeReceiver;
---
+++ koeskoes/0.Giftle/React/src/components/Seller/HomeSeller.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+function HomeSeller() {
+  return (
+    <div>
+      <h1>HOME - Seller</h1>
+    </div>
+  );
+}
+
+export default HomeSeller;
