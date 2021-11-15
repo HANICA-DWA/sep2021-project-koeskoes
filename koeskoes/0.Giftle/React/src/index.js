28327ac3497280c77e20adbce3eee10fd3816278
SWLdeBruin <38204216+SWLdeBruin@users.noreply.github.com>
2021-11-15 15:02:34 +0100

    Front-end video uploaden toegevoegd

--- koeskoes/0.Giftle/React/src/index.js
+++ koeskoes/0.Giftle/React/src/index.js
@@ -1,14 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-
-import './styles/index.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
-
-import App from './components/App';
-import React from "react";
-import ReactDOM from "react-dom";
 import "./styles/index.css";
+
 import * as Redux from "redux";
 import { mainReducer } from "./redux/reducers/mainReducer";
 import thunkMiddleware from "redux-thunk";
