0218f3af9768d8a86de13cb49b8b780e2a793556
Sven <S.vanEe1@student.han.nl>
2021-11-17 15:29:15 +0100

    QRscanner stuk back

--- koeskoes/0.Giftle/src/components/Receiver/ScanQR.jsx
+++ koeskoes/0.Giftle/src/components/Receiver/ScanQR.jsx
@@ -1,30 +1,46 @@
 import React from "react";
-import { Navigate } from "react-router";
 import { useState } from "react";
+import { Navigate } from "react-router";
 import QrReader from "react-qr-reader";
+import "../../styles/receiver/scanQR.css";
 
 function ScanQR() {
   const [delay] = useState(100);
-  const [previewStyle] = useState({ height: 240, width: 320 });
+  const [previewStyle] = useState({ height: `17em`, width: `17em` });
+  const [isError, setIsError] = useState(false);
 
   const handleError = (err) => {
+    setIsError(true);
     console.error(err);
   };
+
+  const errorBox = () => {
+    if (isError) {
+      <div class="alert alert-danger d-flex align-items-center" role="alert">
+        Fout met het de camera of het scannen!
+      </div>;
+      console.log("ERROR");
+    }
+  };
+
   return (
     <div className="vertical-center colored-background">
-      <div className="container text-center rounded p-3 bg-light">
+      <div className="mainBox container text-center rounded p-3 bg-light">
         <h1 className="my-2">Scan uw QR-code</h1>
-        <QrReader
-          delay={delay}
-          style={previewStyle}
-          onError={handleError}
-          onScan={(data) => {
-            if (data) {
-              console.log(data);
-              window.open(data);
-            }
-          }}
-        />
+        {errorBox()}
+        <div className="scanContainer">
+          <QrReader
+            delay={delay}
+            style={previewStyle}
+            onError={handleError}
+            onScan={(data) => {
+              if (data) {
+                console.log(data);
+                window.location.href = data;
+              }
+            }}
+          />
+        </div>
       </div>
     </div>
   );
