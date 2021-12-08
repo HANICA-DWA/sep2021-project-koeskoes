f875c50625497f5979daaa4204e41c87fd11c618
Sven <S.vanEe1@student.han.nl>
2021-12-08 14:23:29 +0100

    CommonFunction commentaar

--- koeskoes/0.Giftle/Express/commonFunctions/generateRandomFileName.js
+++ koeskoes/0.Giftle/Express/commonFunctions/generateRandomFileName.js
@@ -1,8 +1,10 @@
 /**
  * 
- * @param {string} originalFileName 
- * @param {string} random 
- * @returns 
+ * Function that generates a random video filename 
+ * 
+ * @param {string} originalFileName the orginal filename from the database
+ * @param {string} random random string
+ * @returns the final filename and the upload path
  */
 const generateRandomFileName = (originalFileName, random) => {
   const fileNameWithoutExtension = originalFileName.replace(/\.[^/.]+$/, "");
