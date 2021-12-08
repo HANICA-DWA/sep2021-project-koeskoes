c0cf75c4a6f741d2ef6bb7cb47a8cd3e65c66288
Jordi-obermeier <74540009+Jordi-obermeier@users.noreply.github.com>
2021-12-08 14:06:44 +0100

    Video.js comments geschreven

--- koeskoes/0.Giftle/Express/routes/videos.js
+++ koeskoes/0.Giftle/Express/routes/videos.js
@@ -1,17 +1,18 @@
 const express = require("express");
 const router = express.Router();
 const mongoose = require("mongoose");
-const path = require('path');
+const path = require("path");
 require("../model/uploadModel");
 
 const Uploads = mongoose.model("UploadSchema");
 
+/**
+ * if textCode exists, it will fetch data from that textCode record
+ */
 router.get("/:textCode", async (req, res) => {
-  const video = await Uploads
-    .findOne({
-      textCode: req.params.textCode,
-    })
-    .exec();
+  const video = await Uploads.findOne({
+    textCode: req.params.textCode,
+  }).exec();
 
   if (video !== null) {
     res.json(video);
@@ -20,15 +21,16 @@ router.get("/:textCode", async (req, res) => {
   }
 });
 
-router.get('/video/:videoName', async (req, res) => {
-  const video = await Uploads
-    .findOne({
-      videoName: req.params.videoName,
-    })
-    .exec();
+/**
+ * if videoName exists, it will fetch data from that videoName record
+ */
+router.get("/video/:videoName", async (req, res) => {
+  const video = await Uploads.findOne({
+    videoName: req.params.videoName,
+  }).exec();
 
   if (video !== null) {
-    res.sendFile(path.join(__dirname, '../', 'videos/', video.videoName));
+    res.sendFile(path.join(__dirname, "../", "videos/", video.videoName));
   } else {
     res.json({ status: "error", message: "No textcode found" });
   }
