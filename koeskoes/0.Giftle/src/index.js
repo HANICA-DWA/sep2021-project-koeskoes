ddc4977386c9b66d87573c332439fa3eef0df571
SWLdeBruin <38204216+SWLdeBruin@users.noreply.github.com>
2021-12-13 08:40:33 +0100

    Redux dev tools fix

--- koeskoes/0.Giftle/src/index.js
+++ koeskoes/0.Giftle/src/index.js
@@ -16,7 +16,7 @@ const logger = (store) => (next) => (action) => {
 
 export const theStore = Redux.createStore(
   mainReducer,
-  Redux.compose(Redux.applyMiddleware(logger, thunkMiddleware), window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()),
+  Redux.compose(Redux.applyMiddleware(logger, thunkMiddleware), window.devToolsExtension ? window.devToolsExtension() : f => f),
 );
 
 const mainComponent = (
