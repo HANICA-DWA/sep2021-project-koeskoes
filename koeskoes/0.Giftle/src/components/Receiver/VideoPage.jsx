ca710ff3221acc6e97e99720bf772716d6d5b77b
Sven <S.vanEe1@student.han.nl>
2021-12-15 13:54:58 +0100

    watch video receiver page better code

--- koeskoes/0.Giftle/src/components/Receiver/VideoPage.jsx
+++ koeskoes/0.Giftle/src/components/Receiver/VideoPage.jsx
@@ -21,7 +21,7 @@ import { ReactComponent as PlayCircle } from "../../assets/play-circle.svg";
 import { ReactComponent as PauseCircle } from "../../assets/pause-circle.svg";
 import { ReactComponent as ReplayCircle } from "../../assets/arrow-clockwise.svg";
 import { ReactComponent as ShareIcon } from "../../assets/share-fill.svg";
-
+import VideoPlayer from "../Common/VideoPlayer";
 /**
  * Page showing the video (by textCode) for the receiver
  *
@@ -30,17 +30,8 @@ import { ReactComponent as ShareIcon } from "../../assets/share-fill.svg";
 function VideoPage() {
   const { textCode } = useParams();
   const navigate = useNavigate();
-  const dispatch = useDispatch();
   const [videoData, setVideoData] = useState({});
   const [videoError, setVideoError] = useState(null);
-  const [isVideoTime, setIsVideoTime] = useState(null);
-  const [isVideoWatchedTime, setIsVideoWatchedTime] = useState(null);
-  const [minutes, setMinutes] = useState(null);
-  const [seconds, setSeconds] = useState(null);
-  const [progressBar, setProgressBar] = useState(null);
-  const [videoState, setVideoState] = useState(1);
-  const [isLoading, setIsLoading] = useState(true);
-  const watched = useSelector((state) => state.videos.watched);
 
   const [isPopUp, setIsPopUp] = useState(false);
 
@@ -58,132 +49,11 @@ function VideoPage() {
         return setVideoError(true);
       }
     };
-
     getVideo();
   }, [textCode]);
 
-  useEffect(() => {
-    if (!watched) {
-      if (isVideoWatchedTime != null && isVideoTime != null) {
-        if (Math.floor(isVideoWatchedTime) === Math.floor(isVideoTime / 2)) {
-          dispatch(sendVideoWatchedMail(textCode));
-        }
-      }
-    }
 
-    setMinutes(Math.floor(isVideoWatchedTime / 60));
-    setSeconds(
-      Math.floor(isVideoWatchedTime) - Math.floor(isVideoWatchedTime / 60) * 60
-    );
-
-    setProgressBar(
-      <div className="progress">
-        <div
-          title="Tijd"
-          className="progress-bar"
-          role="progressbar"
-          aria-valuenow="1"
-          style={{ width: (100 / isVideoTime) * isVideoWatchedTime + "%" }}
-          aria-valuemin="0"
-          aria-valuemax="100"
-        ></div>
-      </div>
-    );
-  }, [isVideoTime, isVideoWatchedTime, textCode, dispatch, watched]);
-
-  /**
-   *
-   * Video play button with state indicating what button to show
-   *
-   * @param state state with: case 1. "play" - case 2. "pause" - case 3. "replay"
-   * @returns button with the state, indicating what button to show
-   *
-   */
-  const videoPlayButton = (state) => {
-    switch (state) {
-      case 1:
-        return (
-          <button
-            className="btn btn-primary my-3 mx-4"
-            onClick={() => setVideoState(2)}
-          >
-            Afspelen&nbsp;
-            <PlayCircle />
-          </button>
-        );
-      case 2:
-        return (
-          <button
-            className="btn btn-primary my-3 mx-4"
-            onClick={() => setVideoState(1)}
-          >
-            Pauzeren&nbsp;
-            <PauseCircle />
-          </button>
-        );
-      case 3:
-        return (
-          <button
-            className="btn btn-primary my-3 mx-4"
-            onClick={() => setVideoState(2)}
-          >
-            Opnieuw afspelen&nbsp;
-            <ReplayCircle />
-          </button>
-        );
-      default:
-        return null;
-    }
-  };
-
-  /**
-   *
-   * Videoplayer settings for the video
-   *
-   * @returns progressbar with timer, two buttons for the step proces and a button indicating "pause" of "play"
-   *
-   */
-  const videoPlayerSettings = () => {
-    return (
-      <>
-        {progressBar}
-        <div>
-          {(minutes < 10 ? "0" + minutes : minutes) +
-            ":" +
-            (seconds < 10 ? "0" + seconds : seconds === 60 ? "00" : seconds)}
-        </div>
-        {videoPlayButton(videoState)}
-        <button
-          className="btn btn-primary"
-          onClick={() => navigate("/receiver/reaction")}
-        >
-          Verstuur een reactie&nbsp;
-          <RightArrow />
-        </button>
-      </>
-    );
-  };
-
-  /**
-   *
-   * Loading icon if the video is still loading (fetching data)
-   *
-   * @param loading boolean if true show loading, if false return function
-   * @returns loading spinner or text if loading is true, if loading is false return function
-   *
-   */
-  const loadingPlayer = (loading) => {
-    if (loading) {
-      return (
-        <div className="spinner-border" role="status">
-          <span className="visually-hidden">Loading...</span>
-        </div>
-      );
-    } else {
-      return videoPlayerSettings();
-    }
-  };
-
+  
   const showPopUp = () => {
     const videoURL = `http://localhost:3000/receiver/watchSharedVideo/${textCode}`;
     if (isPopUp) {
@@ -251,31 +121,27 @@ function VideoPage() {
     if (videoError === true) {
       return (
         <>
-          <h1>Er is geen video gevonden met deze code.</h1>
-          <button className="btn btn-primary">Terug</button>
+          <h2>Er is geen video gevonden met deze code.</h2>
         </>
       );
     }
     if (videoError === false) {
       return (
         <>
-          <h2>Videoboodschap voor {videoData.nameReceiver}</h2>
-          <ReactPlayer
-            url={
-              "http://localhost:4000/api/videos/video/" + videoData.videoName
-            }
-            width="100%"
-            height="100%"
-            playing={videoState === 2 ? true : false}
-            progressInterval={100}
-            onReady={() => setIsLoading(false)}
-            onEnded={() => setVideoState(3)}
-            onDuration={(time) => setIsVideoTime(time)}
-            onProgress={({ playedSeconds }) =>
-              setIsVideoWatchedTime(playedSeconds)
+          <VideoPlayer
+            title={`Videoboodschap voor ${videoData.nameReceiver}`}
+            url="http://localhost:4000/api/videos/video/"
+            videoCreationPath={
+              `http://localhost:4000/api/orders/order/` + textCode
             }
           />
-          {loadingPlayer(isLoading)}
+          <button
+            className="btn btn-primary reactionButton"
+            onClick={() => navigate("/receiver/reaction")}
+          >
+            Verstuur een reactie&nbsp;
+            <RightArrow />
+          </button>
           <hr />
           <div className="text-start float-start">
             <h3>Afzender:</h3>
