b2d7d5680fd052d1ca3af60e5c63944955507476
Sven <S.vanEe1@student.han.nl>
2021-12-20 14:11:50 +0100

    Spinner message

--- koeskoes/0.Giftle/src/components/Common/Spinner.jsx
+++ koeskoes/0.Giftle/src/components/Common/Spinner.jsx
@@ -1,8 +1,13 @@
 const Spinner = () => {
   return (
-    <div className="spinner-border my-5" role="status">
-      <span className="visually-hidden">Loading...</span>
-    </div>
+    <>
+      <div className="spinner-border my-5" role="status">
+        <span className="visually-hidden">Loading...</span>
+      </div>
+      <div classname="loading-text" role="status">
+        <h5>Een moment geduld a.u.b.</h5>
+      </div>
+    </>
   );
 };
 
